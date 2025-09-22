76e1435bb5592416e27ba94bcaf9ea57c7e79d9c
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2025-09-22 22:15:18 +0530

    added support for mdx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./page/home.jsx";
 import Layout from "./layout";
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import Test from "./page/test.mdx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Layout>
-      <Home />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mdx" element={<Test />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-
+import mdx from '@mdx-js/rollup'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    {enforce: 'pre', ...mdx()},
+
+    react({include: /\.(jsx|js|mdx|md)$/}), tailwindcss()],
    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
